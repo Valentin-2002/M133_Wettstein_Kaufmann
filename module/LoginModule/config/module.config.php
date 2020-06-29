568be061b1005aeea96a16db2ce529772b37a1ae
Valentin Wettstein <valentin.wettstein@santismail.ch>
2020-06-29 07:42:08 +0200

    Initialized more Stuff

--- module/LoginModule/config/module.config.php
+++ module/LoginModule/config/module.config.php
@@ -4,12 +4,35 @@ namespace LoginModule;
 
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
+
+
 return [
     'controllers' => [
         'factories' => [
             Controller\LoginModuleController::class => InvokableFactory::class,
         ],
     ],
+
+        // The following section is new and should be added to your file:
+        'router' => [
+            'routes' => [
+                'login' => [
+                    'type'    => Segment::class,
+                    'options' => [
+                        'route' => '/login[/:action[/:id]]',
+                        'constraints' => [
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'id'     => '[0-9]+',
+                        ],
+                        'defaults' => [
+                            'controller' => Controller\LoginModuleController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+
     'view_manager' => [
         'template_path_stack' => [
             'login' => __DIR__ . '/../view',
