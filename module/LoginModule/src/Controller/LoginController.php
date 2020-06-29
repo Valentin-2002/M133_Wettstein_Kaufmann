c63b1e04e3bcc8291fdf85e2ed3b667ef2f5a4d7
Valentin Wettstein <valentin.wettstein@santismail.ch>
2020-06-29 08:17:49 +0200

    Reconfigured Project Structure

---
+++ module/LoginModule/src/Controller/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LoginModule\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class LoginController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function loginAction()
+    {
+    }
+    
+}
